5a4e0cdb0ca283306ac3f434eb4cf81763c7a417
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-11-09 09:24:03 -0500

    Initial Commit. Created backdrop for poster, created simple poster cover.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/", (request, response, next) => {
+  response.render("home.ejs");
+});
+
+app.get("/about", (request, response, next) => {
+  response.render("about.ejs");
+});
+
+app.get("/photos", (request, response, next) => {
+  response.render("photos.ejs");
+});
+
+app.listen(3000);
